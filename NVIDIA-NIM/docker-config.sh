c417c3c70048367508a01efc884070608e55cdc4
jatolentino <jose_antoniotv@hotmail.com>
2022-08-22 01:18:27 -0500

    docker-config.sh changed

---
+++ NVIDIA-NIM/docker-config.sh
@@ -0,0 +1,12 @@
+docker login nvcr.io
+export NGC_API_KEY=<PASTE_API_KEY_HERE>
+export LOCAL_NIM_CACHE=~/.cache/nim
+mkdir -p "$LOCAL_NIM_CACHE"
+docker run -it --rm \
+    --gpus all \
+    --shm-size=16GB \
+    -e NGC_API_KEY \
+    -v "$LOCAL_NIM_CACHE:/opt/nim/.cache" \
+    -u $(id -u) \
+    -p 8000:8000 \
+    nvcr.io/nim/meta/llama-3.1-405b-instruct:1.1.2
